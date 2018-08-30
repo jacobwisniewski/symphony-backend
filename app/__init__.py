d97380700310a431829b0e18ea6ac1b97880a144
stevenhvtran <stransteven@outlook.com>
2018-08-30 19:48:36 +1000

    Added more hello world code

--- app/__init__.py
+++ app/__init__.py
@@ -9,8 +9,10 @@ app = Flask(__name__)
 # Enable Cross Origin Resource Sharing
 CORS(app)
 
-# Register backends
+# Enable RESTful module
 api = Api(app)
+
+# Register API Endpoints
 api.add_resource(backend.Hello, '/api')
 
 
--- app/backend.py
+++ app/backend.py
@@ -4,3 +4,6 @@ from flask_restful import Resource
 class Hello(Resource):
     def get(self):
         return {'hello': 'world'}
+
+    def post(self):
+        return {'something': 'else'}
