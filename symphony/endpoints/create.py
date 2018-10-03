90f9025dabecdf2fb98efb864c227cb69358cd26
stevenhvtran <stransteven@outlook.com>
2018-10-03 13:01:24 +1000

    Restructured create to not accept access tokens

--- symphony/endpoints/create.py
+++ symphony/endpoints/create.py
@@ -6,8 +6,8 @@ from symphony.utils import random_string
 
 # Parser for /api/create
 parser = reqparse.RequestParser(bundle_errors=True)
-parser.add_argument('access_code', type=str, default=None)
-parser.add_argument('mongo_id', type=str, default=None)
+parser.add_argument('mongo_id', type=str, required=True, default=None,
+                    help='Mongo ID is required')
 parser.add_argument('latitude', type=float, default=None)
 parser.add_argument('longitude', type=float, default=None)
 parser.add_argument('gig_name', required=True, type=str,
@@ -16,30 +16,6 @@ parser.add_argument('private', required=True, type=bool,
                     help='Specify if Gig is private')
 parser.add_argument('algorithm', required=True, type=str,
                     help='Type of algorithm is required')
-
-
-def get_user(args):
-    """Gets the MongoID from user's JSON parameters
-
-    The user is created or updated in the database if an access code was sent
-    in the parameters
-    :param args: JSON data that was sent to /api/create
-    :type args: dict
-    :returns: Document of the user in the database
-    :rtype: pymongo.Document
-    """
-    # Get the mongo_id for the user
-    if args['access_code']:
-        # Add/update user in database
-        tokens = spotify.get_tokens(args['access_code'], 'create')
-        profile = spotify.get_user_profile(tokens)
-        top_songs = spotify.get_top_songs(tokens)
-        mongo_id = spotify_user.create_update(tokens, profile, top_songs)
-    else:
-        mongo_id = args['mongo_id']
-
-    users = Collection('users')
-    return users[mongo_id]
 
 
 def update_user(args, playlist_url, user):
@@ -80,17 +56,12 @@ class Create(Resource):
         """
         args = parser.parse_args()
 
-        # Checks that either access code or mongo id were provided
-        if not args['access_code'] and not args['mongo_id']:
-            abort(400, 'Invite code or Mongo ID required')
-            return
-
         # Uses provided credentials to get a user from the database
-        try:
-            user = get_user(args)
-        except spotify.LoginError:
+        users = Collection('users')
+        user = users[args['mongo_id']]
+
+        if not user:
             abort(401, 'Invalid credentials')
-            return
 
         gigs = Collection('gigs')
 
