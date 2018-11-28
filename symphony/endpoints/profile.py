6d6d0b7d1a41cf011be5ea2a6115404ccecf3852
stevenhvtran <stransteven@outlook.com>
2018-11-28 11:51:49 +1100

    Added error checking for access codes

--- symphony/endpoints/profile.py
+++ symphony/endpoints/profile.py
@@ -28,9 +28,13 @@ def get_user_data(args):
             redirect_uri=f'{config.FRONTEND_URL}/profile/callback',
             scope='user-library-read,user-top-read'
         )
-        tokens = auth.get_access_token(args['access_code'])
-        if not tokens:
-            return None
+
+        # Attempt to get access token with code provided
+        try:
+            tokens = auth.get_access_token(args['access_code'])
+        except spotipy.oauth2.SpotifyOauthError:
+            abort(401, 'Invalid credentials')
+            return
 
         client = spotipy.client.Spotify(tokens['access_token'])
         current_user = client.current_user()
